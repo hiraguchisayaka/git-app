42c4a05fbf2f946d937abda4f5b9ebcca75ae771
hiraguchisayaka <59869369+hiraguchisayaka@users.noreply.github.com>
2020-01-14 23:42:30 +0900

    delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,22 +7,16 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
-  # GET /articles/1
-  # GET /articles/1.json
   def show
   end
 
-  # GET /articles/new
   def new
     @article = Article.new
   end
 
-  # GET /articles/1/edit
   def edit
   end
 
-  # POST /articles
-  # POST /articles.json
   def create
     @article = Article.new(article_params)
 
@@ -51,8 +45,6 @@ class ArticlesController < ApplicationController
     end
   end
 
-  # DELETE /articles/1
-  # DELETE /articles/1.json
   def destroy
     @article.destroy
     respond_to do |format|
